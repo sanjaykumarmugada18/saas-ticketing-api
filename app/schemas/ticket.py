b179c29bff98800e3abc32f3a89c4ba67dea4cb3
sanjaykumarmugada18 <mugadasanjay@gmail.com>
2026-06-24 20:54:43 +0530

    feat(tickets): create pydantic schemas for ticket validation and response

---
+++ app/schemas/ticket.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from pydantic import BaseModel, ConfigDict
+from app.models.ticket import TicketStatus
+
+# 1. Shared properties (Keeps our code DRY)
+class TicketBase(BaseModel):
+    title: str
+    description: str
+
+# 2. Properties to receive on ticket creation
+class TicketCreate(TicketBase):
+    pass # 'pass' means it just inherits the base fields and adds nothing else
+
+# 3. Properties to return to the client
+class TicketResponse(TicketBase):
+    id: int
+    status: TicketStatus
+    created_at: datetime
+    owner_id: int
+
+    model_config = ConfigDict(from_attributes=True)
